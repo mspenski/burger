b36027bcf02a4f5ca13b8a98fcfb5118434e0d3a
mspenski <44811974+mspenski@users.noreply.github.com>
2019-02-05 19:23:37 -0500

    linking to heroku

--- server.js
+++ server.js
@@ -1,35 +1,27 @@
 var express = require("express");
 
+var PORT = process.env.PORT || 3000;
+
 var app = express();
 
-// Set the port of our application
-// process.env.PORT lets the port be set by Heroku
-var PORT = process.env.PORT || 8080;
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
 
-// Sets up the Express app to handle data parsing
+// Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Set Handlebars.
 var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var mysql = require("mysql");
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "root",
-  database: "burgers_db"
-});
+app.use(routes);
 
-connection.connect(function (err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-
-  console.log("connected as id " + connection.threadId);
+app.listen(PORT, function () {
+  console.log("App now listening at localhost:" + PORT);
 });
