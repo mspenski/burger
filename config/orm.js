b23099dad070df5d981a73d19161f2e88e51f1e4
mspenski <44811974+mspenski@users.noreply.github.com>
2019-02-12 11:54:20 -0500

    made some styling changes, going to try to get heroku working

--- config/orm.js
+++ config/orm.js
@@ -23,14 +23,11 @@ function objToSql(ob) {
   // loop through the keys and push the key/value as a string int arr
   for (var key in ob) {
     var value = ob[key];
-    // check to skip hidden properties
     if (Object.hasOwnProperty.call(ob, key)) {
-      // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
       if (typeof value === "string" && value.indexOf(" ") >= 0) {
         value = "'" + value + "'";
       }
-      // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
-      // e.g. {sleepy: true} => ["sleepy=true"]
+
       arr.push(key + "=" + value);
     }
   }
@@ -70,7 +67,6 @@ var orm = {
       cb(result);
     });
   },
-  // An example of objColVals would be {name: panther, sleepy: true}
   update: function(table, objColVals, condition, cb) {
     var queryString = "UPDATE " + table;
 
@@ -90,5 +86,4 @@ var orm = {
   }
 };
 
-// Export the orm object for the model (cat.js).
 module.exports = orm;
--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,5 @@
+DROP DATABASE IF EXISTS burgers_db;
+
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
@@ -7,3 +9,5 @@ CREATE TABLE burgers (
   devoured BOOLEAN DEFAULT false,
   PRIMARY KEY (id)
 );
+
+INSERT INTO burgers (burger_name) VALUES ("Jalapeno Burger", "Big Mac", "Bacon Cheeseburger")
